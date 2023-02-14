6ea73ec1108707a0ec7eb4535a27f75a3719a7f3
Alessio Balsini <balsini@google.com>
2023-02-14 10:13:39 +0800

    BACKPORT: Reduce the OverlayFS code by making the flag conversion function generic and reusable.
    OverlayFS implements its own function to translate iocb flags into rw
    flags, so that they can be passed into another vfs call.
    With commit ce71bfea207b4 ("fs: align IOCB_* flags with RWF_* flags")
    Jens created a 1:1 matching between the iocb flags and rw flags,
    simplifying the conversion.
    
    ref -> https://lore.kernel.org/all/20210118192748.584213-2-balsini@android.com/

--- fs/overlayfs/file.c
+++ fs/overlayfs/file.c
@@ -213,22 +213,7 @@ static void ovl_file_accessed(struct file *file)
 	touch_atime(&file->f_path);
 }
 
-static rwf_t ovl_iocb_to_rwf(struct kiocb *iocb)
-{
-	int ifl = iocb->ki_flags;
-	rwf_t flags = 0;
-
-	if (ifl & IOCB_NOWAIT)
-		flags |= RWF_NOWAIT;
-	if (ifl & IOCB_HIPRI)
-		flags |= RWF_HIPRI;
-	if (ifl & IOCB_DSYNC)
-		flags |= RWF_DSYNC;
-	if (ifl & IOCB_SYNC)
-		flags |= RWF_SYNC;
-
-	return flags;
-}
+#define OVL_IOCB_MASK (IOCB_DSYNC | IOCB_HIPRI | IOCB_NOWAIT | IOCB_SYNC)
 
 static ssize_t ovl_read_iter(struct kiocb *iocb, struct iov_iter *iter)
 {
@@ -246,7 +231,8 @@ static ssize_t ovl_read_iter(struct kiocb *iocb, struct iov_iter *iter)
 
 	old_cred = ovl_override_creds(file_inode(file)->i_sb);
 	ret = vfs_iter_read(real.file, iter, &iocb->ki_pos,
-			    ovl_iocb_to_rwf(iocb));
+			    iocb_to_rw_flags(iocb->ki_flags,
+						     OVL_IOCB_MASK));
 	ovl_revert_creds(old_cred);
 
 	ovl_file_accessed(file);
@@ -281,7 +267,7 @@ static ssize_t ovl_write_iter(struct kiocb *iocb, struct iov_iter *iter)
 	old_cred = ovl_override_creds(file_inode(file)->i_sb);
 	file_start_write(real.file);
 	ret = vfs_iter_write(real.file, iter, &iocb->ki_pos,
-			     ovl_iocb_to_rwf(iocb));
+			     iocb_to_rw_flags(iocb->ki_flags, OVL_IOCB_MASK));
 	file_end_write(real.file);
 	ovl_revert_creds(old_cred);
 
